3e72ca62fe56ab3c9b059b6bc77641abca4ad5af
Adriano Ruberto <adriano.ruberto@heig-vd.ch>
2017-04-04 17:58:29 +0200

    Fake

--- calculatrice/src/calculator/parser/Expr.scala
+++ calculatrice/src/calculator/parser/Expr.scala
@@ -4,6 +4,7 @@ package calculator.parser
 sealed trait Expr
 
 object Expr {
+
 	/** A number literal */
 	case class Literal(value: Double) extends Expr
 
@@ -14,7 +15,19 @@ object Expr {
 	case class Assign(name: String, value: Expr) extends Expr
 
 	/** A binary operation */
-	case class Binary(operator: String, lhs: Expr, rhs: Expr) extends Expr
+	case class Binary(operator: String, lhs: Expr, rhs: Expr) extends Expr /*{
+		// Fake official names
+		private def operatorName: String = operator match {
+			case "+" => "Plus"
+			case "-" => "Minus"
+			case "/" => "Divide"
+			case "*" => "Times"
+			case "%" => "Mod"
+			case "^" => "Pow"
+			case o => o
+		}
+		override def toString: String = s"$operatorName($lhs,$rhs)"
+	} */
 
 	/** An unary operation */
 	case class Unary(operator: String, operand: Expr) extends Expr
